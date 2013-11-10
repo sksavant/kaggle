d95c62f0bd77255eea7767f7e8a37ea9e74bed9d
Savant Krishna <savant.2020@gmail.com>
2013-11-10 21:49:37 +0530

    misc

--- src/meanvar.py
+++ src/meanvar.py
@@ -14,6 +14,18 @@ xyz = [[x],[y],[z]]
 while True:
     l = trainfile.readline()
     if l=="":
+        mean = (sum(xyz[0])/len(xyz[0]),sum(xyz[1])/len(xyz[1]),sum(xyz[2])/len(xyz[2]))
+        var = []
+        for i in range(3):
+            vsum = 0
+            for xval in xyz[0]:
+                vsum = vsum+ (xval-mean[i])**2
+            var.append(vsum/len(xyz[0]))
+        devicemeanvar[cur_dev]  = (mean[0],mean[1],mean[2],var[0],var[1],var[2]) 
+        print devicemeanvar[cur_dev]
+        pr = str(devicemeanvar[cur_dev])
+        pr = pr[1:len(pr)-1]
+        meanfile.write(str(cur_dev)+", "+pr+"\n")
         break
     t,x,y,z,dev = map(float,l.split(","))
     dev = int(dev)
